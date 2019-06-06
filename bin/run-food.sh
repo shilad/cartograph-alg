ddfae4fa771e7cd9e9f8e843fba5dd7277bd84c5
luli2949 <F8ortune2949>
2019-06-06 13:44:11 -0500

    fix commands for popularity

--- bin/run-food.sh
+++ bin/run-food.sh
@@ -7,8 +7,11 @@
 #
 # Run this script using ./bin/run-food.sh
 
-python -m cartograph.domain_concept_creator data/food Food_and_drink 5000
+#python -m cartograph.domain_concept_creator data/food Food_and_drink 5000
+
 # Add label_creator command here
-# Add vector_creator command here
+python -m cartograph.vector_creator data/food data/original_vectors
 # Add popularity_creator command here
-python -m cartograph.xy_embeddings_builder data/food
+#python -m cartograph.popularity_creator data/food food
+
+python -m cartograph.xy_embeddings_builder data/food article_vectors.csv
