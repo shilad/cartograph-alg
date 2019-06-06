c5f1fbc0ad764be88e9033a1d34c781053961e08
luli2949 <F8ortune2949>
2019-06-06 11:52:06 -0500

    popularity creator

--- cartograph/popularity_creator.py
+++ cartograph/popularity_creator.py
@@ -1,10 +1,18 @@
+"""
+Given a domain concept, gets the popularity score for each article in that domain concept,
+produces a csv file that contains article ids, article names, and popularity scores.
+
+This implementation uses Wikipedia's PageView API.
+
+Author: Lu Li
+"""
 import pageviewapi as pv
 import pandas as pd
 import datetime
-import sys
 import os
 
-def get_page_views(page_name):
+
+def get_popularity_score(page_name):
     """Get number of views of a page for a specific food using PageView API from Wikipedia."""
     today = datetime.date.today().strftime('%Y%m%d')
     last_year_today = (datetime.date.today() - datetime.timedelta(days=365)).strftime('%Y%m%d')
@@ -19,30 +27,37 @@ def get_page_views(page_name):
 pop_list = []
 
 
-def create_popularity(wikiproject):
+def create_popularity_score(project_name):
 
-    domain_concepts = pd.read_csv('/Users/research/Documents/Projects/cartograph-alg/data/' + wikiproject + '/domain_concept.csv')
+    domain_concepts = pd.read_csv('/Users/research/Documents/Projects/cartograph-alg/data/' + project_name + '/domain_concept.csv')
     for index, row in domain_concepts.iterrows():
         try:
-            pop = get_page_views(row['article_name'])
-            pop_list.append(pop)
+            popularity_score = get_popularity_score(row['article_name'])
+            pop_list.append(popularity_score)
         except:
             pop_list.append('na')
     domain_concepts["popularity_score"] = pop_list
-    return domain_concepts
+    return domain_concepts.iloc[:, 1:]
 
 
-def create_csv(wikiproject, directory):
-    df = create_popularity(wikiproject)
+def create_csv(project_name, directory):
+    df = create_popularity_score(project_name)
     df.to_csv(directory + '/popularity_score.csv', index_label='article_id')
 
 
-def main():
-    if not os.path.exists(sys.argv[1]):
-        os.mkdir(sys.argv[1])
-    # articles = create_popularity(sys.argv[2])
-    create_csv(sys.argv[2], sys.argv[1])
+def main(map_directory, project_name):
+    if not os.path.exists(map_directory):
+        os.makedirs(map_directory)
+    create_csv(project_name, sys.argv[1])
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+
+    if len(sys.argv) != 3:
+        sys.stderr.write('Usage: %s map_directory project_name number_of_articles' % sys.argv[0])
+        sys.exit(1)
+
+    map_directory, project_name = sys.argv[1:]
+    main(map_directory, project_name)
+
