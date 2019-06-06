fa16d316623be59f7de0f160a11ca67d215e8239
Summer Research <research@mscs256-28384.stu.macalester.edu>
2019-06-06 10:45:12 -0500

    Added id to front and removed index.

--- cartograph/vector_creator.py
+++ cartograph/vector_creator.py
@@ -1,9 +1,8 @@
-import csv
-from collections import defaultdict
 import pandas as pd
 import time
 
 vectors = {}
+
 
 def to_float(string):
     try:
@@ -23,11 +22,6 @@ domain_concept_df = pd.read_csv('../data/food/domain_concept.csv')
 temp_map_dict = {}# for fast look up
 for i, row in domain_concept_df.iterrows():
     temp_map_dict[row['article_name'].replace(" ", "_")] = row['article_id']
-#print()
-
-# with open('../data/food/domain_concept.csv', encoding="ISO-8859-1") as data:
-#     articles = [row[0] for row in csv.reader(data, delimiter='\n')]
-#     articles[:] = [x.replace(" ", "_") for x in articles]
 
 article_set = set(temp_map_dict.keys())# O(1) look up.
 article_vectors_df_ready = []
@@ -35,7 +29,6 @@ for food_article in vectors.keys():
     if food_article in article_set:
         article_vectors_df_ready.append([food_article]+vectors[food_article])
 
-#print(len(article_vectors_df_ready))
 
 vector_ids = ['vector_'+str(i) for i in range(100)]# we know the size of the vectors previously
 
@@ -46,16 +39,27 @@ for i,row in domain_concept_df.iterrows():
 
 # a Dataframe expects a dictionary where {col:[list of values in column]
 article_w_vectors = pd.DataFrame(article_vectors_df_ready, columns=['article_name']+vector_ids)
-for i,row in article_w_vectors.iterrows():
-    #article_w_vectors.insert(0, 'article_id', temp_map_dict[row['article_name']])
-    article_w_vectors.loc[i, 'article_id'] = temp_map_dict[row['article_name']]
+for i, row in article_w_vectors.iterrows():
+    if i == 0:
+        article_w_vectors.insert(0, 'article_id', temp_map_dict[row['article_name']])
+    else:
+        article_w_vectors.loc[i, 'article_id'] = temp_map_dict[row['article_name']]
 
 print(article_w_vectors.head())
-article_w_vectors.to_csv("../data/article_vectors.csv") # get article ids from domain_concepts file
+article_w_vectors.to_csv("../data/article_vectors.csv", index=False) # get article ids from domain_concepts file
 
 
-
-
+#
+# def main():
+#     if not os.path.exists(sys.argv[1]):
+#         os.mkdir(sys.argv[1])
+#     urls = create_urls(sys.argv[2], sys.argv[3])
+#     articles = create_domain_concept(urls)
+#     create_csv(articles, sys.argv[1])
+#
+#
+# if __name__ == '__main__':
+#     main()
 
 
 
