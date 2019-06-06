13c7abd17a856470af41f81d3e4c0102475d28c6
luli2949 <F8ortune2949>
2019-06-06 12:01:12 -0500

    Popularity creator

--- bin/run-food.sh
+++ bin/run-food.sh
@@ -11,4 +11,6 @@ python -m cartograph.domain_concept_creator data/food Food_and_drink 5000
 # Add label_creator command here
 # Add vector_creator command here
 # Add popularity_creator command here
+python -m cartograph.popularity_creator data/food food
+
 python -m cartograph.xy_embeddings_builder data/food article_vectors.csv
