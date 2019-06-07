eca2ad1d176e0008bfd14d5ff141b1549e227d6a
luli2949 <F8ortune2949>
2019-06-07 14:55:36 -0500

    run

--- bin/run-food.sh
+++ bin/run-food.sh
@@ -11,12 +11,13 @@
 # python -m cartograph.label_creator data/food
 # python -m cartograph.vector_creator data/food data/original_vectors
 
-python -m cartograph.popularity_creator data/food
+# python -m cartograph.popularity_creator data/food
 
 # python -m cartograph.xy_embeddings_builder data/food
 # python -m cartograph.cluster_builder data/food
-# python -m cartograph.json_generator data/food
-# python -m cartograph.filter_articles data/food
-# python -m cartograph.label_selector data/food
+python -m cartograph.label_selector data/food
 
-#python -m cartograph.svg_generator data/food 1000 1000
+# python -m cartograph.json_generator data/food/
+# python -m cartograph.filter_articles data/food
+
+# python -m cartograph.svg_generator data/food 800 800
