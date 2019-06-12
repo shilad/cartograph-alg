706f0d5a302e4f267e59f358ad99f85a2e853915
luli2949 <F8ortune2949>
2019-06-12 13:37:13 -0500

    fixed bug and adjusted output format

--- evaluation/cluster_validation_metrics.py
+++ evaluation/cluster_validation_metrics.py
@@ -1,53 +1,81 @@
 """
-Internal evaluation methods on clustering algorithms.
+Internal evaluation metrics on validating clustering algorithms.
 """
-import numpy as np
 import pandas as pd
-from sklearn.metrics import normalized_mutual_info_score
-import functools
 import sklearn.metrics as metrics
 from s_dbw import S_Dbw
-from hdbscan import HDBSCAN
-
-from scipy import stats
 
 
+def get_sdbw_score(xy_list, labels):
+    # return S Dbw validity index, the smaller the better a cluster is.
+    return S_Dbw(xy_list, labels)
 
-def db_score(xy_list, labels):
-    # return davies bouldin score, the smaller the better a cluster is.
+
+def get_db_score(xy_list, labels):
+    # return Davies-Bouldin index, the smaller the better a cluster is.
     return metrics.cluster.davies_bouldin_score(xy_list, labels)
 
 
-def silhouette_score(xy_list, labels):
-    # return silhouette score, the larger the better a cluster is.
+def get_silhouette_score(xy_list, labels):
+    # return silhouette index, the larger the better a cluster is.
     return metrics.cluster.silhouette_score(xy_list, labels)
 
 
-def ch_score(xy_list, labels):
-    # return silhouette score, the larger the better a cluster is.
-    return metrics.cluster.silhouette_score(xy_list, labels)
+def get_ch_score(xy_list, labels):
+    # return Calinski Harabasz index, the larger the better a cluster is.
+    return metrics.cluster.calinski_harabasz_score(xy_list, labels)
 
 
 def read_cluster(map_directory, method):
     original = pd.read_csv(map_directory + '/cluster_groups_' + method + '.csv').sort_values(by=['article_id'])['country']
+    return original
 
 
-
-def main(map_directory, method):
+def get_data_points(map_directory, method):
     datapoints = pd.read_csv(map_directory + '/article_vectors_' + method + '.csv').drop(columns='article_name')
-    X = datapoints.values
-    original_cluster = read_cluster(map_directory, method)
-    print(ch_score(X, original_cluster))
+    return datapoints.values
 
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) != 3:
-        sys.stderr.write('Usage: %s map_directory vector_directory' % sys.argv[0])
-        sys.exit(1)
+def compare_across_metrics(map_directory, algorithm_1, algorithm_2):
+    X_1 = get_data_points(map_directory, algorithm_1)
+    cluster_1 = read_cluster(map_directory, algorithm_1)
+    sdbw_score_1 = get_sdbw_score(X_1, cluster_1)
+    silhouette_score_1 = get_silhouette_score(X_1, cluster_1)
+    ch_score_1 = get_ch_score(X_1, cluster_1)
+    db_score_1 = get_db_score(X_1, cluster_1)
 
-    map_directory, method = sys.argv[1:]
-    main(map_directory,  method)
+    X_2 = get_data_points(map_directory, algorithm_2)
+    cluster_2 = read_cluster(map_directory, algorithm_2)
+    sdbw_score_2 = get_sdbw_score(X_2, cluster_2)
+    silhouette_score_2 = get_silhouette_score(X_2, cluster_2)
+    ch_score_2 = get_ch_score(X_2, cluster_2)
+    db_score_2 = get_db_score(X_2, cluster_2)
+
+    if sdbw_score_1 > sdbw_score_2:
+        print(algorithm_2 + " gives better clustering according to s_DBw index. ")
+    else:
+        print(algorithm_1 + " gives better clustering according to s_DBw index. ")
+
+    if silhouette_score_1 > silhouette_score_2:
+        print(algorithm_1 + " gives better clustering according to silhouette index. ")
+    else:
+        print(algorithm_2 + " gives better clustering according to silhouette index. ")
+
+    if db_score_1 > db_score_2:
+        print(algorithm_2 + " gives better clustering according to Davies-Bouldin index. ")
+    else:
+        print(algorithm_1 + " gives better clustering according to Davies-Bouldin index. ")
+
+    if ch_score_1 > ch_score_2:
+        print(algorithm_1 + " gives better clustering according to Calinski Harabasz index. ")
+    else:
+        print(algorithm_2 + " gives better clustering according to Calinski Harabasz index. ")
+
+    return
 
 
 
 
+map_directory = '../data/food'
+method = 'original'
+
+compare_across_metrics(map_directory, "original", "combined")
