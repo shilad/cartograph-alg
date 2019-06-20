d92567a63f5fdb4aff0749cbace9cf0785bfcaf8
lirvin123 <lily.irvin@gmail.com>
2019-06-20 15:22:20 -0500

    working label evaluation

--- cartograph/evaluation/label_evaluation.py
+++ cartograph/evaluation/label_evaluation.py
@@ -18,40 +18,41 @@ def find_matches(gold_standard, alg):
     synonym_count = 0
 
     for index, row in gold_standard.iterrows():
-        truth = row['label']
-        truth_stems = []
-        truth_synonyms = []
+        if str(index) in alg.keys():
+            truth = row['label']
+            truth_stems = []
+            truth_synonyms = []
 
-        if " " in truth:
-            words = word_tokenize(truth)
-            for word in words:
+            if " " in truth:
+                words = word_tokenize(truth)
+                for word in words:
 
-                stem = get_stem(word)
-                if stem is not None:
-                    truth_stems.append(stem)
+                    stem = get_stem(word)
+                    if stem is not None:
+                        truth_stems.append(stem)
 
-                synonyms = get_synonyms(word)
-                if synonyms is not None:
-                    for synonym in synonyms:
-                        truth_synonyms.append(synonym)
+                    synonyms = get_synonyms(word)
+                    if synonyms is not None:
+                        for synonym in synonyms:
+                            truth_synonyms.append(synonym)
 
-        alg_label = alg[str(index)]["Country"]
+            alg_label = alg[str(index)]["Country"]
 
-        if " " in alg_label:
-            if (set(alg_label.split())).issubset(set(truth_stems)):
-                exact_count += 1
-                print(truth, alg_label, "exact")
-            if (set(alg_label.split())).issubset(set(truth_synonyms)):
-                synonym_count += 1
-                print(truth, alg_label, "synonyms")
+            if " " in alg_label:
+                if (set(alg_label.split())).issubset(set(truth_stems)):
+                    exact_count += 1
+                    print(truth, alg_label, "exact")
+                if (set(alg_label.split())).issubset(set(truth_synonyms)):
+                    synonym_count += 1
+                    print(truth, alg_label, "synonyms")
 
-        else:
-            if (set(alg_label)).issubset(set(truth_stems)):
-                exact_count += 1
-                print(truth, alg_label, "exact")
-            if (set(alg_label)).issubset(set(truth_synonyms)):
-                synonym_count += 1
-                print(truth, alg_label, "synonym")
+            else:
+                if (set(alg_label)).issubset(set(truth_stems)):
+                    exact_count += 1
+                    print(truth, alg_label, "exact")
+                if (set(alg_label)).issubset(set(truth_synonyms)):
+                    synonym_count += 1
+                    print(truth, alg_label, "synonym")
     return exact_count, synonym_count
 
 
@@ -73,9 +74,9 @@ def main(gold_standard_csv, alg_json):
     with open(alg_json) as file:
         alg_labels = json.load(file)
     exact_matches, synonym_matches = find_matches(gold_standard_labels, alg_labels)
-    print("There were " + str(exact_matches) + " articles labeled exactly.")
-    print("There were " + str(synonym_matches) + " articles labeled with a synonym or synonyms of the true label.")
+    print(str(json.dumps({'num_exact_matches': exact_matches})))
+    print(str(json.dumps({'num_synonym_matches': synonym_matches})))
 
 
 if __name__ == '__main__':
-    main('../data/gold_standard_labels.csv', '../experiments/culture/0000/domain.json')
+    main('/Users/research/Documents/Projects/cartograph-alg/data/culture/gold_standard_labels.csv', '/Users/research/Documents/Projects/cartograph-alg/experiments/culture/0002/domain.json')
