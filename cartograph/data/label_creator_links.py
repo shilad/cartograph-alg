f5138c51dcd3549c9f7350c8bbe909659cd61d16
yurenpang <ypang@macalester.edu>
2019-06-25 16:46:54 -0500

    get links from wikipedia api

--- cartograph/data/label_creator_links.py
+++ cartograph/data/label_creator_links.py
@@ -28,20 +28,24 @@ def fetch_links_from_api(domain_concept):
     # edges cases: if wikitext is empty, doesn't contain *
     #              if wikitext is null
     #              if links is empty
-    parse = data['parse']
 
-    if 'wikitext' in parse:
-        wikitext_versions = len(parse['wikitext'])
-        if wikitext_versions == 1:
-            wiki_text = parse['wikitext']['*'][parse['wikitext']['*'].find("'''"):]
-            return re.findall("(?<=\[\[)(.+?)(?=[|(\]\])])", wiki_text)
+    if 'parse' in data:
+        parse = data['parse']
+
+        if 'wikitext' in parse:
+            wikitext_versions = len(parse['wikitext'])
+            if wikitext_versions == 1:
+                wiki_text = parse['wikitext']['*'][parse['wikitext']['*'].find("'''"):]
+                return re.findall("(?<=\[\[)(.+?)(?=[|(\]\])])", wiki_text)
+            else:
+                logging.warning('%s: found multiple wikitext.', domain_concept)
         else:
-            logging.warning('%s: found multiple wikitext.', domain_concept)
+            if 'title' in parse:
+                logging.warning("%s: Couldn't find wikitext. ", domain_concept)
+            else:
+                logging.warning("%s: couldn't find anything. ", domain_concept)
     else:
-        if 'title' in parse:
-            logging.warning("%s: Couldn't find wikitext. ", domain_concept)
-        else:
-            logging.warning("%s: couldn't find anything. ", domain_concept)
+        logging.warning("%s: couldn't be parsed", domain_concept)
 
     return []
 
@@ -55,8 +59,10 @@ def create_links(domain_concept_csv):
     links_to_id = {}
     df = pd.read_csv(domain_concept_csv)
     rows_list = []
+    count = 0
 
     for index, row in df.iterrows():  # test df.head(10).iterrows()
+        if count == 500: print("This is %d th article", count)
         article_id = row[0]
         domain_concept = row[1]
         for link in fetch_links_from_api(domain_concept):
@@ -64,6 +70,7 @@ def create_links(domain_concept_csv):
                 links_to_id[link] = len(links_to_id)
             id = links_to_id.get(link, len(links_to_id))
             rows_list.append({"article_id": article_id, "label_id": id})
+        count = count+1
 
     return links_to_id, pd.DataFrame(rows_list)
 
@@ -71,11 +78,11 @@ def create_links(domain_concept_csv):
 def create_link_id_str_csv(directory, links_to_ids):
     id_to_link = [(id, link) for (link, id) in links_to_ids.items()]
     links_df = pd.DataFrame(id_to_link, columns=["label_id", "label"])
-    links_df.to_csv(directory + '/category_names.csv', index=False)
+    links_df.to_csv(directory + '/link_names.csv', index=False)
 
 
 def create_article_link_csv(article_link_df, directory):
-    article_link_df.to_csv(directory + "/article_categories.csv", index=False)
+    article_link_df.to_csv(directory + "/article_links.csv", index=False)
 
 
 # def main(map_directory):
