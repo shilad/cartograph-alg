0606107a412d6b44ef74c69aaf500f968d9e08c4
luli2949 <F8ortune2949>
2019-06-25 16:37:15 -0500

    deleted redundant line spaces

--- cartograph/cluster_builder.py
+++ cartograph/cluster_builder.py
@@ -48,9 +48,7 @@ def get_kmeans(article_vectors, k):
         dist.append(spatial.distance.cosine(a, b))
         # dist.append(np.linalg.norm(a - b))
     df_with_centroids['distance'] = dist
-
     df_with_centroids = df_with_centroids[['country', 'article_id', 'distance']]
-
     return df_with_centroids
 
 
