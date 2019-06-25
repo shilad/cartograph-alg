3c370cdad95e414d8b51b2a112bd7bd4c550e011
lionely <lionelscotty95@gmail.com>
2019-06-25 15:37:23 -0500

    so I can pull

---
+++ bin/make-culture-map.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Example shell script to create a map dataset
+# Before running this, you must activate Python.
+#
+# For example: source ~/venv/cartograph-alg/bin/activate
+#
+# Note: this script does not call domain_concept_creator.py because the domain concept was scraped from a different
+# location (Shilad's friend Aaron's file from WikiMedia)
+#
+
+set -e
+
+python -m cartograph.data.label_creator_categories data/culture
+python -m cartograph.data.label_creator_keywords data/culture
+python -m cartograph.data.label_creator_hierarchical_cat data/culture
+python -m cartograph.data.vector_creator data/culture data/original_nav_vectors.txt.bz2
+python -m cartograph.datapopularity_creator data/culture
