7ba8264fbf4aabff9e727f31ec8cfe4555b204e3
luli2949 <F8ortune2949>
2019-06-26 11:39:57 -0500

    reordered functions; replaced hard coding vector length with calculating it by using number of columns

--- cartograph/cluster_builder.py
+++ cartograph/cluster_builder.py
@@ -8,48 +8,18 @@ Author: Lu Li
 """
 import sys
 import math
-
 from scipy import spatial
 from sklearn.cluster import KMeans
 import pandas as pd
 import warnings
 import argparse
 import numpy as np
-# import numpy.linalg.norm
+
 with warnings.catch_warnings():
     # filter sklearn\externals\joblib\parallel.py:268:
     # DeprecationWarning: check_pickle is deprecated
     warnings.simplefilter("ignore", category=DeprecationWarning)
     from hdbscan import HDBSCAN
-
-def get_kmeans(article_vectors, k):
-    """
-
-    :param article_vectors: vectors to cluster
-    :param k: desired number of clusters for kmeans
-    :return: a dataframe two columns: article id and country
-    """
-    article_id = article_vectors['article_id']
-    # creates a 4004 x 100 matrix
-    matrix = article_vectors.iloc[:, 1:].values
-    kmeans = KMeans(n_clusters=int(k)).fit(matrix[0:len(matrix)])
-    article_vectors['country'] = kmeans.labels_
-    # creates a 4004 x 2 dataframe with article id and the country it belongs to.
-    df = pd.DataFrame(article_vectors, columns=['country'])
-    df['article_id'] = article_id
-    centroids = pd.DataFrame(list(kmeans.cluster_centers_))
-    centroids.index.name = 'country'
-    df_with_vectors = pd.merge(df, article_vectors.drop(columns='country'), on='article_id')
-    df_with_centroids = pd.merge(df_with_vectors, centroids, on='country')
-    dist = []
-    for index, row in df_with_centroids.iterrows():
-        a = np.array(row[2:102])
-        b = np.array(row[102:202])
-        dist.append(spatial.distance.cosine(a, b))
-        # dist.append(np.linalg.norm(a - b))
-    df_with_centroids['distance'] = dist
-    df_with_centroids = df_with_centroids[['country', 'article_id', 'distance']]
-    return df_with_centroids
 
 
 def mag(x):
@@ -72,6 +42,37 @@ def normalize_vectors(dataframe):
     return dataframe
 
 
+def get_kmeans(article_vectors, k):
+    """
+
+    :param article_vectors: vectors to cluster
+    :param k: desired number of clusters for kmeans
+    :return: a dataframe two columns: article id and country
+    """
+    article_id = article_vectors['article_id']
+    # creates a 4004 x 100 matrix
+    matrix = article_vectors.iloc[:, 1:].values
+    kmeans = KMeans(n_clusters=int(k)).fit(matrix[0:len(matrix)])
+    article_vectors['country'] = kmeans.labels_
+    # creates a 4004 x 2 dataframe with article id and the country it belongs to.
+    df = pd.DataFrame(article_vectors, columns=['country'])
+    df['article_id'] = article_id
+    centroids = pd.DataFrame(list(kmeans.cluster_centers_))
+    centroids.index.name = 'country'
+    df_with_vectors = pd.merge(df, article_vectors.drop(columns='country'), on='article_id')
+    df_with_centroids = pd.merge(df_with_vectors, centroids, on='country')
+    dist = []
+    for index, row in df_with_centroids.iterrows():
+        num_dims = (len(row) - 2) / 2
+        a = np.array(row[2: int(len(row) - num_dims)])
+        b = np.array(row[int(len(row) - num_dims): len(row)])
+        dist.append(spatial.distance.cosine(a, b))
+        # dist.append(np.linalg.norm(a - b))
+    df_with_centroids['distance'] = dist
+    df_with_centroids = df_with_centroids[['country', 'article_id', 'distance']]
+    return df_with_centroids
+
+
 def get_hdbscan(article_vectors, min_size, noise=False):
     """
 
@@ -80,29 +81,16 @@ def get_hdbscan(article_vectors, min_size, noise=False):
     :return: a dataframe two columns: article id and country
     """
 
-
     article_id = article_vectors['article_id']
     matrix = normalize_vectors(article_vectors.iloc[:, 2:]).values
     hdbscan = HDBSCAN(min_cluster_size=int(min_size), alpha=1.3, cluster_selection_method='leaf', min_samples=8).fit(matrix[0:len(matrix)])
-
-
     article_vectors['country'] = hdbscan.labels_
-
     df = pd.DataFrame(article_vectors, columns=['country'])
     df['article_id'] = article_id
-    count = 0
-
     if noise:
         probabilities = hdbscan.probabilities_
         df['country_likelihood'] = probabilities
-
-    for index, row in df.iterrows():
-        if row['country_likelihood'] == 0:
-            count = count + 1
-    print(count)
     return df
-
-
 
 
 if __name__ == '__main__':
