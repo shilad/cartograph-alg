1f82a8e4a464b367f03ca56bc667d77601013f2a
yurenpang <ypang@macalester.edu>
2019-06-28 11:04:47 -0500

    combine all the labels

---
+++ cartograph/data/label_creator_combined.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import os
+import time
+
+# article_id --> label_id (category, links, keyword)
+# set of labels assign new id
+# for i -- > all article_id
+#     id = get_id from one
+#     for file --> 3, go find all labels in each class
+#         find corresponding id for that row <-- from dictionary to get string
+#         find the id based on this string
+# output the id file
+# output article file
+
+def create_sublabel_dic(label_candidate_csv):
+    df = pd.read_csv(label_candidate_csv)
+    return df.set_index('label_id')['label'].to_dict()
+
+def label_combiner(article_categories, article_keywords, article_links,
+                   categories_names, keyword_names, links_names,
+                   domain_concept_csv):
+    # dictionary correpondings to id : string of original label candidate
+    cat_dic = create_sublabel_dic(categories_names)
+    keyword_dic = create_sublabel_dic(keyword_names)
+    links_dic = create_sublabel_dic(links_names)
+
+    label_candidates_list = [article_categories, article_keywords, article_links]
+    label_candidates_dic = [cat_dic, keyword_dic, links_dic]
+
+    # create complete label candidates
+    total_label_str = list(set(cat_dic.values()) | set(keyword_dic.values()) | set(links_dic.values()))
+    total_label_to_id = dict(map(lambda t: (t[1], t[0]), enumerate(total_label_str)))
+
+    rows_list = []
+    label_to_id = {}
+
+    for index, row in pd.read_csv(domain_concept_csv).head().iterrows():
+        article_id = row[0]
+
+        for df_index, label_candidate_df in enumerate(label_candidates_list):
+            # find all label_id corresponding to the article_id as a list
+            label_ids_lst = list(label_candidate_df.loc[(label_candidate_df.article_id == article_id), 'label_id'])
+            # find the corresponding str in each label candidate dictionary
+            label_strs = [label_candidates_dic[df_index][label_id] for label_id in label_ids_lst]
+
+            for label in label_strs:
+                if label not in label_to_id:
+                    label_to_id[label] = len(label_to_id)
+                id = label_to_id.get(label, len(label_to_id))
+                rows_list.append({"article_id": article_id, "label_id": id})
+
+    return label_to_id, pd.DataFrame(rows_list)
+
+
+def create_link_id_str_csv(directory, links_to_ids):
+    id_to_link = [(id, link) for (link, id) in links_to_ids.items()]
+    links_df = pd.DataFrame(id_to_link, columns=["label_id", "label"])
+    links_df.to_csv(directory + '/link_names.csv', index=False)
+
+
+def create_article_link_csv(article_link_df, directory):
+    article_link_df.to_csv(directory + "/article_links.csv", index=False)
+
+
+def main(map_directory):
+    if not os.path.exists(map_directory):
+        os.makedirs(map_directory)
+
+    start = time.time()
+    links_to_id, link_df = create_links(map_directory + "/domain_concept.csv")
+    logging.warning("Time Spent: %.3f", time.time() - start)
+    create_article_link_csv(link_df, map_directory)
+    create_link_id_str_csv(map_directory, links_to_id)
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 2:
+        sys.stderr.write('Usage: %s map_directory' % sys.argv[0])
+        sys.exit(1)
+
+    map_directory = sys.argv[1]
+    main(map_directory)
+
+article_categories = pd.read_csv("../../data/food/article_categories.csv")
+article_keywords = pd.read_csv("../../data/food/article_keywords_summary.csv")
+article_links = pd.read_csv("../../data/food/article_links.csv")
+
+print(label_combiner(article_categories, article_keywords, article_links, '../../data/food/category_names.csv', '../../data/food/keyword_summary_names.csv',
+                     '../../data/food/link_names.csv', '../../data/food/domain_concept.csv'))
+
+
+
+
+
