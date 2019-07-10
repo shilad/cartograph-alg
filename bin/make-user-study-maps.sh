f4f39239cc6a2037b5bfe5da0445ebaf133685d5
yurenpang <ypang@macalester.edu>
2019-07-10 09:46:10 -0500

    internet media and technology keywords

--- bin/make-user-study-maps.sh
+++ bin/make-user-study-maps.sh
@@ -11,20 +11,20 @@ set -e
 set -x
 
 
-projects=(Media Technology Internet)
-directories=(media technology internet)
+projects=(Technology Internet)
+directories=(technology internet)
 
 
-for i in {0..2}
+for i in {0..1}
 do
     echo ${projects[$i]}
-#    python -m cartograph.data.domain_concept_creator data/${directories[$i]} ${projects[$i]} 5000
-#    python -m cartograph.data.label_creator_categories data/${directories[$i]}
-#    python -m cartograph.data.label_creator_hierarchical_cat data/${directories[$i]}
-#    python -m cartograph.data.label_creator_links data/${directories[$i]}
+    python -m cartograph.data.domain_concept_creator data/${directories[$i]} ${projects[$i]} 5000
+    python -m cartograph.data.label_creator_categories data/${directories[$i]}
+    python -m cartograph.data.label_creator_hierarchical_cat data/${directories[$i]}
+    python -m cartograph.data.label_creator_links data/${directories[$i]}
     python -m cartograph.data.text_getter data/${directories[$i]}
     python -m cartograph.data.label_creator_keywords data/${directories[$i]}
-#    python -m cartograph.data.label_creator_key-phrases data/${directories[$i]}
-#    python -m cartograph.data.vector_creator data/${directories[$i]} data/original_nav_vectors.txt.bz2
-#    python -m cartograph.data.popularity_creator data/${directories[$i]}
+    python -m cartograph.data.label_creator_key-phrases data/${directories[$i]}
+    python -m cartograph.data.vector_creator data/${directories[$i]} data/original_nav_vectors.txt.bz2
+    python -m cartograph.data.popularity_creator data/${directories[$i]}
 done
