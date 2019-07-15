464244fa51df3a03976ad31e3a56425615dd2edd
luli2949 <F8ortune2949>
2019-07-15 10:01:32 -0500

    Adjusted to fit with updated parameters

--- bin/joint_kmeans_umap.sh
+++ bin/joint_kmeans_umap.sh
@@ -20,7 +20,7 @@ source ./bin/experiment-utils.sh
 
 # Step 1: Get the experiment id. This is *not* map specific.
 # An experiment id can be used for multiple maps.
-exp_id=0036
+exp_id=0052
 
 # $(get_experiment_id)
 
@@ -40,7 +40,11 @@ xy_embedding tsne
  python -m umap+kmeans.joint_umap_kmeans \
         --experiment ${exp_dir} \
         --vectors ${exp_dir}/vanilla_vectors.csv \
-        --k 8
+        --k 8 \
+        --weight_high 10 \
+        --weight_low 1 \
+        --num_neighbors 20 >> ${exp_dir}/evaluation.json
+
 
 
 python -m cartograph.label_selector \
@@ -48,17 +52,30 @@ python -m cartograph.label_selector \
         --articles_to_labels data/food/article_categories.csv \
         --label_names data/food/category_names.csv \
         --percentile 0.5 \
-        --label_score tfidf
+        --label_score tfidf \
+        --cluster_groups /original_cluster_groups.csv \
+        --output_file /original_country_labels.csv
+
+
+python -m cartograph.label_selector \
+       --experiment ${exp_dir} \
+        --articles_to_labels data/food/article_categories.csv \
+        --label_names data/food/category_names.csv \
+        --percentile 0.5 \
+        --label_score tfidf \
+        --cluster_groups /cluster_groups.csv \
+        --output_file /country_labels.csv
+
 
 
 # Step 6: Generate JSON
-python -m cartograph.json_generator data/food ${exp_dir} kk /original_cluster_groups.csv /original_xy_embeddings.csv /original_domain.json
-python -m cartograph.json_generator data/food ${exp_dir} kk /cluster_groups.csv /original_xy_embeddings.csv /new_domain.json
+python -m cartograph.json_generator data/food ${exp_dir} kk  /original_country_labels.csv /original_cluster_groups.csv /original_xy_embeddings.csv /original_domain.json
+python -m cartograph.json_generator data/food ${exp_dir} kk /country_labels.csv /cluster_groups.csv /xy_embeddings.csv /new_domain.json
 
 
 # Step 7: Run evaluation metrics and generate HTML & SVG
-python -m cartograph.svg_generator ${exp_dir} 1500 1500 muted /original_domain.json /original_graph.svg
-python -m cartograph.svg_generator ${exp_dir} 1500 1500 muted /new_domain.json /new_graph.svg
+python -m cartograph.svg_generator ${exp_dir} 1500 1500 muted /original_domain.json /original_graph.svg /original_country_labels.csv
+python -m cartograph.svg_generator ${exp_dir} 1500 1500 muted /new_domain.json /new_graph.svg /country_labels.csv
 
 
 # Step 8: Write evaluation metrics
@@ -70,5 +87,16 @@ python -m cartograph.evaluation.xy_embedding_validation ${exp_dir} /xy_embedding
 python -m cartograph.evaluation.cluster_validation_metrics --experiment ${exp_dir} \
                                                            --vectors ${exp_dir}/vanilla_vectors.csv \
                                                            --groups ${exp_dir}/cluster_groups.csv >>${exp_dir}/evaluation.json
+
+python -m cartograph.evaluation.modularity_evaluator --experiment ${exp_dir} \
+                                                     --xy_embeddings_csv ${exp_dir}/original_xy_embeddings.csv \
+                                                     --method nn \
+                                                     --cluster_groups_csv ${exp_dir}/original_cluster_groups.csv >>${exp_dir}/evaluation.json
+
+    python -m cartograph.evaluation.modularity_evaluator --experiment ${exp_dir} \
+                                                     --xy_embeddings_csv ${exp_dir}/xy_embeddings.csv \
+                                                     --method nn \
+                                                     --cluster_groups_csv ${exp_dir}/cluster_groups.csv >>${exp_dir}/evaluation.json
+
 
 python -m cartograph.html_generator ${exp_dir}
