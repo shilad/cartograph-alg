d0054490eb3eb41cfa946e4b7a53ac3f35be9176
luli2949 <F8ortune2949>
2019-07-16 16:35:18 -0500

    added quantifiers

--- cartograph/xy_embed/umap_embed.py
+++ cartograph/xy_embed/umap_embed.py
@@ -9,10 +9,10 @@ import os
 import argparse
 
 
-def create_embeddings(vector_directory, spread, tw, clusters=None):
+def create_embeddings(vector_directory, spread=20.0, tw=0.5, clusters=None):
     df = pd.read_csv(vector_directory)
-    if clusters:
-        cluster_groups = pd.read_csv(clusters)
+    if clusters is not None:
+        cluster_groups = clusters  # pd.read_csv(clusters)
         df = pd.merge(df, cluster_groups, on='article_id')
         points = umap.UMAP(metric='cosine', spread=spread, target_weight=tw).fit_transform(df.iloc[:, 1:-1], y=df.iloc[:, -1])
     else:
@@ -23,7 +23,7 @@ def create_embeddings(vector_directory, spread, tw, clusters=None):
 
 def create_csv(xy_embeddings, map_directory):
     df = pd.DataFrame(xy_embeddings, columns=['article_id', 'x', 'y'])
-    df.to_csv(map_directory + '/xy_embeddings.csv', index=False)
+    df.to_csv(map_directory + '/original_xy_embeddings.csv', index=False)
 
 
 def main(map_directory, vector_directory, spread, tw, clusters):
