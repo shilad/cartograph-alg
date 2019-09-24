2d30f7506e363f5c14799e6ac5bcf10344959ef8
ypang@macalester.edu <sl89616132Aa+>
2019-09-23 22:39:53 -0500

    linear regression.

--- regression/calc_response.py
+++ regression/calc_response.py
@@ -7,39 +7,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def draw_regre(X, Y):
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.15, random_state=42)
+
+def run_linear_regression(x, y):
     model = LinearRegression()
-    reg = model.fit(X.reshape(-1, 1), Y)
-
-    pred = model.predict(X_test.reshape(-1, 1))
-    rms = sqrt(mean_absolute_error(y_test, pred))
-
-
-    ## spline
-    weights = np.polyfit(X_train, y_train)
-    model = np.poly1d(weights)
-    pred = model(X_test)
-
-    xp = np.linspace(X_test.min(), X_test.max(), 70)
-    pred_plot = model(xp)
-    plt.scatter(X_test, y_test, facecolor='None', edgecolor='k', alpha=0.3)
-    plt.plot(xp, pred_plot)
-    plt.show()
-
+    model.fit(x.values, y.values)
+    coeff_df = pd.DataFrame(model.coef_, x.columns, columns=['coefficient'])
+    print(coeff_df)
     # this is linear regression
-    return rms
+    return ""
+
+def run_spline(x, y):
 
 
-def main(hit_labels_csv, tfidf_csv):
-    hit_labels_df = pd.read_csv(hit_labels_csv)
-    tfidf_df = pd.read_csv(tfidf_csv)
 
-    avg_borda = hit_labels_df[["name", "avg_borda"]]
-    tfidf = tfidf_df[["name", "tfidf"]]  # change "name" to the correct label
+def main(hit_labels_csv):
+    data = pd.read_csv(hit_labels_csv)
+    df = data[['h_cat', 'key_phrases', 'key_words', 'lda_label', 'links', 'augmented', 'LDA', "plain"]].astype('category')
+    df[['tfidf', 'pmi', 'avg_borda']] = data[['tfidf', 'pmi', 'avg_borda']]
 
-    df = pd.merge(avg_borda, tfidf, left_on="name", right_on="name")
-    Y = df["avg_borda"]
-    X = df.drop(columns=['avg_borda'])
+    print(df)
 
-    draw_regre(X, Y)
+    y = df["avg_borda"]
+    x = df[['tfidf', 'pmi', 'h_cat', 'key_phrases', 'key_words', 'lda_label', 'links', 'augmented', 'LDA', "plain"]]
+
+    print(x)
+
+    run_linear_regression(x, y)
+
+main("./labels_for_ml_study.csv")
