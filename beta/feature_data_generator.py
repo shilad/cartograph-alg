b2598d1bdffb6ec0ace522f11f2442135be626bc
ypang@macalester.edu <sl89616132Aa+>
2019-11-19 18:20:40 -0600

    debug mode.

--- beta/feature_data_generator.py
+++ beta/feature_data_generator.py
@@ -5,13 +5,11 @@ import pandas as pd
 import argparse
 
 
-def main(experiment_dir, article_labels, cluster_groups, label_source, output_file):
+def main(experiment_dir, article_labels, cluster_groups, label_source, output_file, num_top_labels):
     tfidf = utils.calc_tfidf(article_labels, cluster_groups, ['article_id', 'label_id', 'label', 'tfidf', 'num_countries', 'country', 'country_label_count', 'num_country_labels', 'num_articles', 'label_count', 'label_count_project', 'tf', 'idf'])
-    top_labels = utils.get_top_labels(tfidf)
+    top_labels = utils.get_top_labels(tfidf, num_top_labels)
     top_labels[label_source] = 1
     top_labels.to_csv(experiment_dir + "/" + output_file)
-    # country_labels = article_labels.drop(columns=['article_id']).drop_duplicates()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Select labels for cluster.')
@@ -21,6 +19,7 @@ if __name__ == '__main__':
     parser.add_argument('--label_source', required=True)
     parser.add_argument('--cluster_groups', required=True)
     parser.add_argument('--output_file', required=True)
+    parser.add_argument('--num_top_labels', required=True, type=int)
 
     args = parser.parse_args()
 
@@ -29,4 +28,4 @@ if __name__ == '__main__':
     cluster_groups = pd.read_csv(args.experiment + "/" + args.cluster_groups)
     article_labels = pd.merge(article_labels, label_names, on='label_id')
 
-    main(args.experiment, article_labels, cluster_groups, args.label_source, args.output_file)
+    main(args.experiment, article_labels, cluster_groups, args.label_source, args.output_file, args.num_top_labels)
