1785350b94f82d593586d9915cd6dba35f95c925
luli2949 <13612124718@163.com>
2020-07-07 00:53:56 -0500

    fixed candidate labels

--- cartograph/user_study_label.py
+++ cartograph/user_study_label.py
@@ -7,6 +7,15 @@ This script creates the user study candidate labels for each map.
 
 
 def union_label_candidates(path, k, project):
+    # get top k labels from both label algorithms and take the union
+    all_labels = pd.read_csv(path + "/check.csv")
+    all_labels['complex'] = all_labels['tf'] * all_labels['idf'] * all_labels['sum']
+    all_labels['simple'] = all_labels['tf'] * all_labels['idf']
+    all_labels = all_labels[['country', 'new_name', 'simple', 'complex']].drop_duplicates()
+    simple = all_labels.sort_values('simple', ascending=False).groupby("country").head(k).sort_values('country')
+    complex = all_labels.sort_values('complex', ascending=False).groupby("country").head(k).sort_values('country')
+    union = pd.concat([simple, complex]).drop_duplicates().sort_values('country').reset_index().drop(columns=['index'])
+    # add dummy labels
     dum = pd.read_csv("./data/" + project + "/dummy_labels.csv")
     used = set()
     dummy = pd.DataFrame()
@@ -18,15 +27,8 @@ def union_label_candidates(path, k, project):
                 i = random.randint(0, dum.shape[0] - 1)
                 dum_label = dum.iloc[:, 1][i]
             used.add(dum_label)
-            dummy = dummy.append({'country': int(k), 'new_name': dum_label, 'simple': 0, 'complex': 0, 'isDummy': True},
-                                 ignore_index=True)
-    all_labels = pd.read_csv(path + "/check.csv")
-    all_labels['complex'] = all_labels['tf'] * all_labels['idf'] * all_labels['sum']
-    all_labels['simple'] = all_labels['tf'] * all_labels['idf']
-    all_labels = all_labels[['country', 'new_name', 'simple', 'complex']].drop_duplicates()
-    simple = all_labels.sort_values('simple', ascending=False).groupby("country").head(k).sort_values('country')
-    complex = all_labels.sort_values('complex', ascending=False).groupby("country").head(k).sort_values('country')
-    union = pd.concat([simple, complex]).drop_duplicates().sort_values('country').reset_index().drop(columns=['index'])
+            dummy = dummy.append({'country': int(k), 'new_name': dum_label, 'simple': 0, 'complex': 0, 'isDummy': True}, ignore_index=True)
+
     u = pd.DataFrame(pd.concat([union, dummy]))[['country', 'new_name', 'simple', 'complex', 'isDummy']]
     u = u.astype({"country": int})
     u.to_csv(path + "/candidate_labels.csv")
@@ -45,6 +47,3 @@ if __name__ == '__main__':
     union_label_candidates(args.experiment, args.num_top_labels, args.project)
 
 
-
-project = "food"
-candi = pd.read_csv("/Users/luli/PycharmProjects/cartograph-alg/experiments/food/0296/candidate_labels.csv")
